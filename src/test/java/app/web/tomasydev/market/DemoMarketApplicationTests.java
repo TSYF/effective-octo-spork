2261643929d5f0c2e6d868754029394529d3a196
TSYF <tsyf1999@gmail.com>
2022-10-26 00:44:54 -0300

    first

---
+++ src/test/java/app/web/tomasydev/market/DemoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package app.web.tomasydev.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
